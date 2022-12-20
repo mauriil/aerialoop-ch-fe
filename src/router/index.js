bb3ff9bdf1f1f5218a6c1ed918564f595eb714a5
Mauri Lahitte <mauriciolahitte@Mauricios-MacBook-Air.local>
2022-12-20 16:09:18 -0300

    routing logic

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import FlightDetailView from "../views/FlightDetailView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: HomeView,
+      path: "/:pathMatch(.*)*",
+      name: "flightDetail",
+      component: FlightDetailView,
     },
   ],
 });
